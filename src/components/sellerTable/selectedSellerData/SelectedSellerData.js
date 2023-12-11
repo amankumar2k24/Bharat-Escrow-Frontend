f224a31fc6b9e4e99556fbdb352d2e24f2197b47
aman.kumar2k15@gmail.com <aman.kumar2k15@gmail.com>
2023-12-11 18:51:13 +0530

    changes in dashboard and created sellerTable

--- src/components/sellerTable/selectedSellerData/SelectedSellerData.js
+++ src/components/sellerTable/selectedSellerData/SelectedSellerData.js
@@ -41,7 +41,7 @@ const SelectedSellerData = () => {
         <>
             <form className='relative'>
                 <div className="space-y-12">
-                    <div className="border-b border-gray-900/10 pb-12">
+                    <div className=" pb-12">
                         <h2 className="text-base font-semibold leading-7 text-gray-900">
                             Profile
                         </h2>
--- src/components/table/selectedTableData/SelectedTableData.js
+++ src/components/table/selectedTableData/SelectedTableData.js
@@ -25,7 +25,7 @@ const SelectedTableData = () => {
         <>
             <form>
                 <div className="space-y-12">
-                    <div className="border-b border-gray-900/10 pb-12">
+                    <div className=" pb-12">
                         <h2 className="text-base font-semibold leading-7 text-gray-900">
                             Profile
                         </h2>
